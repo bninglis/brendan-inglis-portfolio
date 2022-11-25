a99ebb9ada71f23424fe94fd18c745f476c44c86
bninglis <107963185+bninglis@users.noreply.github.com>
2022-11-25 00:49:50 -0800

    adds subtitle animation

--- src/components/Menu/Subtitle/Subtitle.js
+++ src/components/Menu/Subtitle/Subtitle.js
@@ -1,16 +1,23 @@
 import "./Subtitle.scss";
+import { useState } from "react";
 
 export default function Subtitle({ handleSetPhase }) {
+    const [isRolled, setIsRolled] = useState(false);
     return (
         <div className="subtitle">
-            <button
-                className="subtitle__ribbon"
-                onClick={() => {
-                    handleSetPhase("intro");
-                }}
-            >
-                <h2 className="subtitle__text">Web Developer</h2>
-            </button>
+            <div className={`subtitle__remove${isRolled ? " subtitle__remove--roll" : ""}`}>
+                <button
+                    className="subtitle__ribbon"
+                    onClick={() => {
+                        handleSetPhase("intro");
+                    }}
+                >
+                    <h2 className="subtitle__text">Web Developer</h2>
+                </button>
+                <div className={`subtitle__window${isRolled ? " subtitle__window--roll" : ""}`}>
+                    <div className={`subtitle__roll${isRolled ? " subtitle__roll--roll" : ""}`} />
+                </div>
+            </div>
         </div>
     );
 }
